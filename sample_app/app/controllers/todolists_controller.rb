7fffe8733e7eb26ae793c09b104343273ac9459b
梅澤晴恵 <harueumezawa@HaruenoMacBook-Air.local>
2020-06-26 14:21:34 +0900

    [Finish] 3,Railsを学ぼう-chapter6

---
+++ sample_app/app/controllers/todolists_controller.rb
@@ -0,0 +1,29 @@
+class TodolistsController < ApplicationController
+  def new
+    # Viewへ渡すためのインスタンス変数に空のモデルオブジェクトを生成する。
+  	@list = List.new
+  end
+  # 以下を追加
+    def create
+        # ストロングパラメーターを使用
+         list = List.new(list_params)
+        # DBへ保存する
+         list.save
+        # 詳細画面へリダイレクト
+        redirect_to todolist_path(list.id)
+    end
+
+    def index
+    	@lists = List.all
+    end
+
+    def show
+        @list = List.find(params[:id])
+    end
+    
+    private
+   
+    def list_params
+        params.require(:list).permit(:title, :body)
+    end
+end
