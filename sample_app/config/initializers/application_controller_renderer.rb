150ebbab30c0d456e363fa4b386bac9897cf4996
梅澤晴恵 <harueumezawa@HaruenoMacBook-Air.local>
2020-06-26 16:45:13 +0900

    [Finish] 3,Railsを学ぼう-chapter8

--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'ddbcbd6c54a9e64f817f6a2d43b7f1a8d27b942ded0dbf867d0928730985dcf3b2e6263df8d418d094714924cdf5a564b2a68c9d0a382f506a896bda60f0b775'
